ef50010f5b16dc66667d0826a6f7fca6a3efa698
MuaddhAlsway <muaddhalsway@gmail.com>
2026-04-20 10:36:28 +0300

    commit

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+
+    // 🔥 Fix GSAP issues
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    ScrollTrigger.refresh();
+
   }, [pathname]);
 
   return null;
